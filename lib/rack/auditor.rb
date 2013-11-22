8d53ab254e43b788e525ef2d00a8d48bf7d5870c
Patrick Robertson <patricksrobertson@gmail.com>
2013-11-22 12:29:59 -0500

    Fix dev mode bug.

--- lib/rack/auditor.rb
+++ lib/rack/auditor.rb
@@ -9,31 +9,29 @@ module Rack
     end
 
     def call(env)
-      audit(env) unless @dev_mode
+      unless @dev_mode
+        key    = env['HTTP_X_API_KEY']
+        secret = env['HTTP_X_API_SECRET']
+
+        return forbidden unless key && secret
+
+        response = HTTParty.get "#{@root_uri}?api_key=#{key}&api_secret=#{secret}"
+
+        case response.code
+        when 403
+          forbidden
+        when 404
+          error_code(404, 'Not Found')
+        when 500
+          error_code(500, 'Server Error')
+        when 503
+          error_code(503, 'Maintenance')
+        when 504
+          error_code(504, 'System Down')
+        end
+      end
 
       @app.call(env)
-    end
-
-    def audit(env)
-      key    = env['HTTP_X_API_KEY']
-      secret = env['HTTP_X_API_SECRET']
-
-      return forbidden unless key && secret
-
-      response = HTTParty.get "#{@root_uri}?api_key=#{key}&api_secret=#{secret}"
-
-      case response.code
-      when 403
-        forbidden
-      when 404
-        error_code(404, 'Not Found')
-      when 500
-        error_code(500, 'Server Error')
-      when 503
-        error_code(503, 'Maintenance')
-      when 504
-        error_code(504, 'System Down')
-      end
     end
 
     private
